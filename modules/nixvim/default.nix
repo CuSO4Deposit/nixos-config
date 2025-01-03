b175e5e6bc9b3b321cf6dc86a5e54755c6003223
cocvu <cocvu@nightcord.unknown.tld>
2025-01-03 09:14:44 +0000

    fix: disable mouse

--- modules/nixvim/default.nix
+++ modules/nixvim/default.nix
@@ -37,7 +37,7 @@
     cursorcolumn = true; # highlight current column
     cursorline = true; # highlight current line
     expandtab = true; # input spaces instead of tabs
-    mouse = null; # disable mouse
+    mouse = ""; # disable mouse
     number = true; # show line number
     relativenumber = true; # show line number relative to cursor
     ruler = true; # show line and column number of cursor
