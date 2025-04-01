a1c9dbefbc162fd8664a651f97e09b8aecc63f98
cocvu <cocvu@nightcord.unknown.tld>
2025-04-01 18:01:56 +0000

    add ssh pubkey

--- hosts/proximo.nix
+++ hosts/proximo.nix
@@ -18,6 +18,7 @@
 
   users.users."cuso4d".openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMJD6JpxiKFEThom4/HMchI8S08+Tuxvp04xSLxtMMLH cuso4d"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEgnoIeHJv3VVT9SgOELc0rlnPz+cv4uA2yESbLdJ7Vv cuso4d"
   ];
 
   # This option defines the first version of NixOS you have installed on this particular machine,
