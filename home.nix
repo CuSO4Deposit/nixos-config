0f9c23c256a0af41a03fbcbf0ddbaf4f40a19af4
cocvu <cocvu@nightcord.unknown.tld>
2025-05-09 14:25:27 +0000

    add portal to share dark theme from former system
    ref: https://discourse.nixos.org/t/installing-gnome-changes-color-scheme-for-hyprland/44000

--- home.nix
+++ home.nix
@@ -280,6 +280,14 @@
     systemd.enable = true;
   };
 
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-hyprland
+    ];
+  };
+
   # This value determines the Home Manager release that your configuration is 
   # compatible with. This helps avoid breakage when a new Home Manager release 
   # introduces backwards incompatible changes. 
