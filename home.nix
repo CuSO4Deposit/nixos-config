212f5a80b233a732207f09bb59c0f4e139bca758
cocvu <cocvu@nightcord.unknown.tld>
2025-05-23 12:58:55 +0000

    wofi-tokyonight theme

--- home.nix
+++ home.nix
@@ -130,6 +130,43 @@
     };
   };
   programs.home-manager.enable = true;
+  
+  programs.wofi = {
+    enable = true;
+    # https://github.com/Sit-Back/wofi-tokyonight/blob/0909aacff8d578777e2d2c261a40bf456226e4c8/style.css
+    style = ''
+      window {
+          background-color: rgb(26, 27, 38);
+          border-radius: 0.5em;
+      }
+
+      #input {
+          padding: 0.7em;
+          border-radius: 0.5em 0.5em 0 0;
+          background-color: #2d2e40;
+          color: #c0caf5;
+          border: none;
+      }
+
+      #text {
+          color: #c0caf5;
+      }
+
+      #entry {
+          padding: 0.7em;
+          color: #c0caf5;
+      }
+
+    #entry:selected {
+          background-color: #7aa2f7;
+      } 
+
+    #text:selected {
+          color: #eee;
+          font-weight: bold;
+      }
+    '';
+  };
 
   wayland.windowManager.hyprland = {
     enable = true;
