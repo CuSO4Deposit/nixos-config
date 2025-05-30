75ea88cfe3061a814d5624d3d4b22b6ed3d21262
cocvu <cocvu@nightcord.unknown.tld>
2025-05-30 04:02:04 +0000

    git-hook to nixfmt the files

--- configuration.nix
+++ configuration.nix
@@ -5,8 +5,13 @@
 # NixOS-WSL specific options are documented on the NixOS-WSL repository:
 # https://github.com/nix-community/NixOS-WSL
 
-{ config, lib, pkgs, inputs, ... }:
-
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
 
 {
   nix.gc = {
@@ -15,27 +20,35 @@
     options = "--delete-older-than 30d";
   };
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
   # https://github.com/NixOS/nixpkgs/issues/158356#issuecomment-1556882689
   nix.settings.substituters = lib.mkForce [
     "https://mirrors.ustc.edu.cn/nix-channels/store"
     "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
-  ];  
-  nix.settings.trusted-users = [ "cuso4d" "root" ];
-
-  environment.systemPackages = let
-    oh-cus-zsh = pkgs.callPackage ./derivations/oh-cus-zsh {};
-  in [
-    inputs.cus-nixvim.packages."${pkgs.system}".nvim
-    oh-cus-zsh
-    pkgs.bat
-    pkgs.busybox
-    pkgs.curl
-    pkgs.git
-    pkgs.jq
-    pkgs.nixfmt-rfc-style
-    pkgs.tldr
   ];
+  nix.settings.trusted-users = [
+    "cuso4d"
+    "root"
+  ];
+
+  environment.systemPackages =
+    let
+      oh-cus-zsh = pkgs.callPackage ./derivations/oh-cus-zsh { };
+    in
+    [
+      inputs.cus-nixvim.packages."${pkgs.system}".nvim
+      oh-cus-zsh
+      pkgs.bat
+      pkgs.busybox
+      pkgs.curl
+      pkgs.git
+      pkgs.jq
+      pkgs.nixfmt-rfc-style
+      pkgs.tldr
+    ];
 
   environment.variables.EDITOR = "nvim";
 
@@ -46,18 +59,22 @@
   users.users.cuso4d = {
     isNormalUser = true;
     home = "/home/cuso4d";
-    extraGroups = [ "wheel" "networkmanager" ];
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+    ];
   };
 
   users.defaultUserShell = pkgs.zsh;
 
   programs.direnv = {
+    enable = true;
+    loadInNixShell = true;
+    nix-direnv = {
       enable = true;
-      loadInNixShell = true;
-      nix-direnv = {
-        enable = true;
-        package = pkgs.nix-direnv;
-      };
+      package = pkgs.nix-direnv;
+    };
+    silent = true;
   };
 
   programs.git = {
@@ -72,14 +89,16 @@
       eval "$(direnv hook bash)"
     '';
 
-    ohMyZsh = let
-      oh-cus-zsh = pkgs.callPackage ./derivations/oh-cus-zsh {};
-    in {
-      enable = true;
-      package = oh-cus-zsh;
-      plugins = [ "git" ];
-      theme = "cphoen";
-    };
+    ohMyZsh =
+      let
+        oh-cus-zsh = pkgs.callPackage ./derivations/oh-cus-zsh { };
+      in
+      {
+        enable = true;
+        package = oh-cus-zsh;
+        plugins = [ "git" ];
+        theme = "cphoen";
+      };
 
     shellAliases = {
       alg = "alias | grep";
--- flake.nix
+++ flake.nix
@@ -1,7 +1,11 @@
 {
-  description = "A NixOS Configuration Flake Wrapper";  
+  description = "A NixOS Configuration Flake Wrapper";
 
   inputs = {
+    cus-nixvim = {
+      url = "git+https://codeberg.org/cocvu/cus-nixvim?ref=main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,44 +16,97 @@
       url = "github:nix-community/NUR";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    cus-nixvim = {
-      url = "git+https://codeberg.org/cocvu/cus-nixvim?ref=main";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
   };
 
-  outputs = { self, home-manager, nixpkgs, nixos-wsl, nur, cus-nixvim }@inputs: {
-    nixosConfigurations = let
-        wslConfig = hostname: nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          specialArgs = { inherit inputs; };
-          modules = [
-            ./configuration.nix
-            ./hosts/${hostname}.nix
-            nixos-wsl.nixosModules.wsl
-          ];
-        };
-        wslHostnames = [ "laborari" "lexikos" "proximo" ];
-      in {
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
 
-      nightcord-dynamica = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs; };
-        modules = [
-          ./configuration.nix
-          ./hosts/dynamica.nix
-          nur.modules.nixos.default # This adds the NUR overlay
-          home-manager.nixosModules.home-manager {
-            home-manager.backupFileExtension = "backup";
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.cuso4d = import ./home;
-          }
-        ];
-      };
-    }  // builtins.listToAttrs (map (name: {
+  outputs =
+    {
+      self,
+      home-manager,
+      nixpkgs,
+      nixos-wsl,
+      nur,
+      ...
+    }@inputs:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgs = nixpkgs.legacyPackages.${system};
+      system = "x86_64-linux";
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+    in
+    {
+      checks = forAllSystems (system: {
+        inherit inputs;
+        pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            nixfmt-rfc-style.enable = true;
+          };
+        };
+      });
+
+      devShells = forAllSystems (system: {
+        default = pkgs.mkShell {
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
+          buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
+        };
+      });
+
+      nixosConfigurations =
+        let
+          wslConfig =
+            hostname:
+            nixpkgs.lib.nixosSystem {
+              inherit system;
+              specialArgs = {
+                inherit inputs;
+              };
+              modules = [
+                ./configuration.nix
+                ./hosts/${hostname}.nix
+                nixos-wsl.nixosModules.wsl
+              ];
+            };
+          wslHostnames = [
+            "laborari"
+            "lexikos"
+            "proximo"
+          ];
+        in
+        {
+
+          nightcord-dynamica = nixpkgs.lib.nixosSystem {
+            inherit system;
+            specialArgs = {
+              inherit inputs;
+            };
+            modules = [
+              ./configuration.nix
+              ./hosts/dynamica.nix
+              nur.modules.nixos.default # This adds the NUR overlay
+              home-manager.nixosModules.home-manager
+              {
+                home-manager.backupFileExtension = "backup";
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.users.cuso4d = import ./home;
+              }
+            ];
+          };
+        }
+        // builtins.listToAttrs (
+          map (name: {
             name = "nightcord-${name}";
             value = wslConfig name;
-          }) wslHostnames);
-  };
+          }) wslHostnames
+        );
+    };
 }
