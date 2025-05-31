4644cb98d653659ca50c12ccb0dc317e2ed02b22
cocvu <cocvu@nightcord.unknown.tld>
2025-05-31 14:56:43 +0000

    add dynamica's key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,9 +1,16 @@
 let
-  proximo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBnPi9CTg8uMgIFCr/sQxo8Fmn/wuJioDnb8SiVk7GUw";
-  users = [
-    proximo
+  proximo-host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBnPi9CTg8uMgIFCr/sQxo8Fmn/wuJioDnb8SiVk7GUw";
+  proximo-user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEVOjjy6t6+Eo5CoGRAUM6VSO1Npik9E0UsOXIVMl90E";
+  dynamica-user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL/tpmLxVSyNLRsaUZmCaUC5uPmFhWl17fITU4LAKj+F";
+  hosts = [
+    proximo-host
   ];
+  users = [
+    dynamica-user
+    proximo-user
+  ];
+  all = hosts ++ users;
 in
 {
-  "firefox_bypass.age".publicKeys = users;
+  "firefox_bypass.age".publicKeys = all;
 }
