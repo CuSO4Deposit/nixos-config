e9dfb96b532817692a15f95bf4ebfce9b434648d
cocvu <cocvu@nightcord.unknown.tld>
2025-06-02 08:13:26 +0000

    update wallpapers

--- home/hyprpaper.nix
+++ home/hyprpaper.nix
@@ -10,7 +10,8 @@
     settings =
       let
         wallpapers = [
-          "${config.home.homeDirectory}/Pictures/Wallpapers/果ての望月_kirico_sk_202505301047.jpg"
+          "${config.home.homeDirectory}/Pictures/Wallpapers/果ての望月_kirico_sk.jpg"
+          "${config.home.homeDirectory}/Pictures/Wallpapers/ヒトリノ灯リ_kirico_sk.jpg"
         ];
       in
       {
