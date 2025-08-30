ea74788041c8491fb9350ee7b6626ec093b07e44
cocvu <cocvu@nightcord.unknown.tld>
2025-08-30 11:44:29 +0000

    zeal relies on qt5 and causes rebuild to fail.

--- home/default.nix
+++ home/default.nix
@@ -30,7 +30,7 @@
     wechat-uos
     wemeet
     wofi
-    zeal
+    zeal-qt6
     zotero
 
     # utils
@@ -65,12 +65,12 @@
     ];
   };
 
-  # This value determines the Home Manager release that your configuration is 
-  # compatible with. This helps avoid breakage when a new Home Manager release 
-  # introduces backwards incompatible changes. 
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
   #
-  # You should not change this value, even if you update Home Manager. If you do 
-  # want to update the value, then make sure to first check the Home Manager 
-  # release notes. 
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
   home.stateVersion = "24.11"; # Did you read the comment?
 }
