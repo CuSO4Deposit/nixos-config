3fd63ef9ec9580bcfd1aba7cab7043e72cacc1a4
cocvu <cocvu@nightcord.unknown.tld>
2025-10-31 14:43:11 +0000

    minecraft server

--- hosts/proximo.nix
+++ hosts/proximo.nix
@@ -15,6 +15,28 @@
   networking.firewall.allowedTCPPorts = [ 7777 ];
   networking.hostName = "nightcord-proximo";
 
+  services.minecraft-server = {
+    declarative = true;
+    enable = true;
+    eula = true;
+    openFirewall = true;
+    serverProperties = {
+      allow-cheats = true;
+      difficulty = "hard";
+      gamemode = "survival";
+      level-name = "MOTION_SICKNESS_DEMO_MC_WO_SITAI";
+      max-players = 4;
+      motd = "点我放松一下";
+      online-mode = false;
+      server-port = 25565;
+      white-list = false;
+    };
+    package = pkgs.papermc;
+    whitelist = {
+      "CuSO4D" = "0f5f4275-656f-41e4-b2ef-1a7914c2e5df";
+    };
+  };
+
   services.mysql = {
     enable = true;
     package = pkgs.mariadb;
