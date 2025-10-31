cd951d90d1725f744e18bfb22834401363dd9153
cocvu <cocvu@nightcord.unknown.tld>
2025-10-31 14:29:12 +0000

    rebuild proximo

--- hosts/modules/server.nix
+++ hosts/modules/server.nix
@@ -5,8 +5,13 @@
   ...
 }:
 {
+  networking.networkmanager.enable = true;
   networking.nameservers = [
     "8.8.8.8"
     "114.114.114.114"
+  ];
+
+  users.users.cuso4d.extraGroups = [
+    "networkmanager"
   ];
 }
