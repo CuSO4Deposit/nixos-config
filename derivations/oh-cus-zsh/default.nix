a8283b78e535e09f3a4582d7952aaaed2a4000ab
cocvu <cocvu@nightcord.unknown.tld>
2025-12-25 07:07:22 +0000

    use postInstall for oh-cus-zsh

--- derivations/oh-cus-zsh/default.nix
+++ derivations/oh-cus-zsh/default.nix
@@ -10,63 +10,11 @@ oh-my-zsh.overrideAttrs (
     };
   in
   {
-    # https://github.com/NixOS/nixpkgs/blob/b27ba4eb322d9d2bf2dc9ada9fd59442f50c8d7c/pkgs/by-name/oh/oh-my-zsh/package.nix#L36
     pname = "oh-cus-zsh";
-    installPhase = ''
-      runHook preInstall
 
-      outdir=$out/share/oh-my-zsh
-      template=templates/zshrc.zsh-template
-
-      mkdir -p $outdir
-      cp -r * $outdir
-      cd $outdir
-
-      rm LICENSE.txt
-      rm -rf .git*
-
-      chmod -R +w templates
-
-      # Change the path to oh-my-zsh dir and disable auto-updating.
-      sed -i -e "s#ZSH=\$HOME/.oh-my-zsh#ZSH=$outdir#" \
-             -e 's/\# \(DISABLE_AUTO_UPDATE="true"\)/\1/' \
-       $template
-
-      chmod +w oh-my-zsh.sh
-
-      # Both functions expect oh-my-zsh to be in ~/.oh-my-zsh and try to
-      # modify the directory.
-      cat >> oh-my-zsh.sh <<- EOF
-
-      # Undefine functions that don't work on Nix.
-      unfunction uninstall_oh_my_zsh
-      unfunction upgrade_oh_my_zsh
-      EOF
-
-      # Look for .zsh_variables, .zsh_aliases, and .zsh_funcs, and source
-      # them, if found.
-      cat >> $template <<- EOF
-
-      # Load the variables.
-      if [ -f ~/.zsh_variables ]; then
-          . ~/.zsh_variables
-      fi
-
-      # Load the functions.
-      if [ -f ~/.zsh_funcs ]; then
-        . ~/.zsh_funcs
-      fi
-
-      # Load the aliases.
-      if [ -f ~/.zsh_aliases ]; then
-          . ~/.zsh_aliases
-      fi
-      EOF
-
-      # Load my theme.
-      cp ${cphoen-zsh-theme}/cphoen.zsh-theme $outdir/themes
-
-      runHook postInstall
+    postInstall = ''
+      mkdir -p $out/share/oh-my-zsh/themes
+      cp ${cphoen-zsh-theme}/cphoen.zsh-theme $out/share/oh-my-zsh/themes/cphoen.zsh-theme
     '';
   }
 )
