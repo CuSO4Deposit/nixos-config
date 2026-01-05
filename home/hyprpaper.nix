d3caff90df59bd3dee0ef7cbc9e7e755ae65c327
cocvu <cocvu@nightcord.unknown.tld>
2026-01-05 09:04:58 +0000

    new syntax for hyprpaper (workaround before FOD)
    Hyprland update: https://hypr.land/news/update53
    Disable message: https://github.com/hyprwm/hyprpaper/issues/307

--- home/hyprpaper.nix
+++ home/hyprpaper.nix
@@ -1,5 +1,6 @@
 {
   config,
+  osConfig,
   ...
 }:
 {
@@ -7,15 +8,23 @@
     enable = true;
     settings =
       let
-        wallpapers = [
-          "${config.home.homeDirectory}/Pictures/Wallpapers/果ての望月_kirico_sk.jpg"
-          "${config.home.homeDirectory}/Pictures/Wallpapers/ヒトリノ灯リ_kirico_sk.jpg"
-          "${config.home.homeDirectory}/Pictures/Wallpapers/多分やさしい夜_kirico_sk.jpg"
-        ];
+        hostname = osConfig.networking.hostName;
+        wallpaperPath =
+          if hostname == "nightcord-laborari" then
+            "${config.home.homeDirectory}/Pictures/Wallpapers/ヒトリノ灯リ_kirico_sk.jpg"
+          else if hostname == "nightcord-dynamica" then
+            "${config.home.homeDirectory}/Pictures/Wallpapers/果ての望月_kirico_sk.jpg"
+          else if hostname == "nightcord-lexikos" then
+            "${config.home.homeDirectory}/Pictures/Wallpapers/多分やさしい夜_kirico_sk.jpg"
+          else
+            "";
       in
       {
-        preload = wallpapers;
-        wallpaper = builtins.map (x: ", " + x) wallpapers;
+        wallpaper = {
+          monitor = "";
+          path = wallpaperPath;
+        };
+        splash = false;
       };
   };
 }
