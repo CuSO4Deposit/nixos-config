a330c0e604de67c01ee2812ea8f403976cbe9ccc
cocvu <cocvu@nightcord.unknown.tld>
2026-03-29 10:59:36 +0000

    mm on lexikos

--- hosts/lexikos.nix
+++ hosts/lexikos.nix
@@ -118,6 +118,30 @@ in
     '';
   };
 
+  systemd.timers."mm-config-extract" = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnBootSec = "1min";
+      OnUnitActiveSec = "10min";
+      Unit = "mm-config-extract.service";
+    };
+  };
+  systemd.services."mm-config-extract" = {
+    path = with pkgs; [
+      bash
+      gawk
+      coreutils
+      gnugrep
+    ];
+    script = ''
+      bash /home/cuso4d/source/mm-config/scripts/extract-bestbefore.sh
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      User = "cuso4d";
+    };
+  };
+
   time.timeZone = "Etc/UTC";
 
   virtualisation.docker = {
